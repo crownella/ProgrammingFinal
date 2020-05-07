3d262aa6d0c43c0526b33be988ea511bc0121ff8
crownella <35846857+crownella@users.noreply.github.com>
2020-05-06 22:39:02 -0400

    finally got picking up packages working smoothly!

---
+++ Assets/Package.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Package : MonoBehaviour
+{
+    bool holding;
+    public float speed = 4f;
+
+    Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        
+    }
+    public void PickUp(GameObject pos)
+    {
+        if (!holding)
+        {
+            print("pick up");
+            holding = true;
+
+            this.transform.SetParent(pos.transform);
+            rb.useGravity = false;
+            rb.mass = 0;
+            rb.isKinematic = true;
+            rb.constraints = RigidbodyConstraints.FreezeRotation;
+        }
+    }
+
+    public void Drop()
+    {
+        if (holding)
+        {
+            print("drop");
+            holding = false;
+
+            this.transform.SetParent(null);
+            rb.useGravity = true;
+            rb.isKinematic = false;
+            rb.constraints = RigidbodyConstraints.None;
+        }
+        
+    }
+}
---
+++ Assets/PlayerRayCast.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * This script is attacthed to the camera to cast a ray for interacting with objects
+ */
+public class PlayerRayCast : MonoBehaviour
+{
+    [SerializeField]
+    private int rayLength = 20;
+
+    public GameObject playerHold;
+
+    bool holdingPackage;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray MouseRay = new Ray(this.transform.position, this.transform.forward);
+        Debug.DrawRay(this.transform.position, this.transform.forward, Color.green);
+        RaycastHit hit;
+
+        if (Physics.Raycast(this.transform.position, this.transform.forward, out hit) && hit.distance < rayLength)
+        {
+
+            if (hit.transform.CompareTag("Package"))
+            {
+                if (Input.GetMouseButton(0))
+                {
+                    holdingPackage = true;
+                    hit.transform.GetComponent<Package>().PickUp(playerHold);
+                }
+                else
+                {
+                    if (holdingPackage)
+                    {
+                        holdingPackage = false;
+                        hit.transform.GetComponent<Package>().Drop();
+                    }
+                }
+            }
+
+            
+        }
+
+
+    }
+}
